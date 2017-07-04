7b310fbaae1a0bb8ade787d1d81ea73cec026c84
“git config --global user.name “shaohua1994-9 <3143334932@qq.com>
2017-07-04 20:16:57 +0800

    merge

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 //引入gulp组件
 var gulp = require('gulp');
+var  imagemin = require('gulp-imagemin');
 
 //压缩html
 var htmlmin = require('gulp-htmlmin');
@@ -13,14 +14,29 @@ gulp.task('html',function(){
 	.pipe(gulp.dest('dist/'))
 	.pipe(browserSync.reload({stream:true}))
 })
+//data
+gulp.task('data',function(){
+	gulp.src('src/**/*.json')
+	.pipe(gulp.dest('dist/'))
+	.pipe(browserSync.reload({stream:true}))
+})
+//imagemin 
+
+gulp.task('img', function () {
+    gulp.src('src/img/**/*.*')
+        .pipe(imagemin(
+        	   { progressive: true}
+        	 ))
+        .pipe(gulp.dest('dist/img'))
+       .pipe(browserSync.reload({stream:true}));
+});
 
 //less文件编译、压缩
 var less = require('gulp-less');
 var cssnano = require('gulp-cssnano');
 
 gulp.task('less',function(){
-	gulp.src(['src/less/**/*.less','!src/less/_*.less'])
-	.pipe(less())
+	gulp.src(['src/less/**/*.css','!src/less/_*.css','node_modules/bootstrap/dist/css/bootstrap.min.css'])
 	.pipe(cssnano())
 	.pipe(gulp.dest('dist/css'))
 	.pipe(browserSync.reload({stream:true}))
@@ -31,8 +47,8 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 
 gulp.task('js',function(){
-	gulp.src('src/js/**/*.js')
-	.pipe(concat('app.js'))
+	gulp.src(['src/js/**/*.js','node_modules/jquery/dist/jquery.min.js','node_modules/bootstrap/dist/js/bootstrap.min.js'])
+	// .pipe(concat('app.js'))
 	.pipe(uglify())
 	.pipe(gulp.dest('dist/js'))
 	.pipe(browserSync.reload({stream:true}))
@@ -40,16 +56,16 @@ gulp.task('js',function(){
 
 //监视所有的文件
 
-gulp.task('watch',function(){
+gulp.task('watch',['html','less','js','img'],function(){
 	gulp.watch('src/**/*.html',['html']);
-	gulp.watch('src/less/**/*.less',['less']);
+	gulp.watch('src/less/**/*.css',['less']);
 	gulp.watch('src/js/**/*.js',['js']);
 })
 
 //启动一个web服务，用于同步测试
 var browserSync = require('browser-sync');
 
-gulp.task('server',['html','less','js'],function(){
+gulp.task('server',['html','less','js','img'],function(){
 	browserSync.init({
 		server:{
 			baseDir:'./dist/'
